8c2b6bfdcd2abb4b70e67e12bde44a03ab8ea7e4
dustinvonsandwich <dustiv@uw.edu>
2021-04-03 02:08:57 -0700

    commit

--- minimum.c
+++ minimum.c
@@ -15,10 +15,11 @@
  * @param theArray array to search for min value.
  * @return int value which is minimum value from array.
  */
-int findMin(const int theArray[]) {
+int findMin(const int theArray[], const int theSize) {
 
    int min = theArray[0];
-   for (int i = 0; i < 6; i++) {
+
+   for (int i = 0; i < theSize; i++) {
       if (theArray[i] < min) {
          min = theArray[i];
       }
@@ -33,7 +34,9 @@ int findMin(const int theArray[]) {
 int main() {
 
    const int intSet[6] = {106, 23, 78, 90, 10, 100};
-   const int minValue = findMin(intSet);
+   const size_t n = sizeof(intSet) / sizeof(intSet[0]);
+
+   const int minValue = findMin(intSet, n);
    
    char s[100] = "The minimum value of this array is: ";
 
